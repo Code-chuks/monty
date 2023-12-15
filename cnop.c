104c891bc04ec807cae213144c4ee0fb3c116390
Code-chuks <ezeuzorchukwudi@gmail.com>
2023-12-14 19:26:08 -0800

    Help me Lord

---
+++ cnop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+/**
+  *f_nop- nothing here
+  *@head: head
+  *@counter: counter
+  *Return: no return
+ */
+void f_nop(stack_t **head, unsigned int counter)
+{
+	(void) counter;
+	(void) head;
+}
